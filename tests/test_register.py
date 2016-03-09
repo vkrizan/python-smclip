6a8a3c321301c6b0081975eb6ff757c46e018afb
Viliam Krizan <vkrizan@redhat.com>
2016-03-09 16:24:58 +0100

    add tests for subcommand registering

---
+++ tests/test_register.py
@@ -0,0 +1,134 @@
+
+import pytest
+
+try:
+    import mock
+except ImportError:
+    import unittest.mock as mock
+
+import smclip
+
+
+def test_error_same_default_name_register():
+
+    class Subcommand(smclip.Command):
+        default_name = 'somename'
+
+    class CommandGroup(smclip.CommandGroup):
+
+        def __init__(self, *args, **kwargs):
+            super(CommandGroup, self).__init__(*args, **kwargs)
+
+            self.register(Subcommand)
+            self.register(Subcommand)
+
+    with pytest.raises(RuntimeError) as excinfo:
+        CommandGroup()
+
+    assert 'already registered' in excinfo.value.message
+
+
+def test_error_same_name_register():
+
+    class Subcommand1(smclip.Command):
+        default_name = 'somename'
+
+    class Subcommand2(smclip.Command):
+        default_name = 'somename2'
+
+    class CommandGroup(smclip.CommandGroup):
+
+        def __init__(self, *args, **kwargs):
+            super(CommandGroup, self).__init__(*args, **kwargs)
+
+            self.register(Subcommand1, name='samename')
+            self.register(Subcommand2, name='samename')
+
+    with pytest.raises(RuntimeError) as excinfo:
+        CommandGroup()
+
+    assert 'already registered' in excinfo.value.message
+
+
+def test_error_no_default_name():
+
+    class SubcommandNonName(smclip.Command):
+        pass
+
+    class CommandGroup(smclip.CommandGroup):
+
+        def __init__(self, *args, **kwargs):
+            super(CommandGroup, self).__init__(*args, **kwargs)
+
+            self.register(SubcommandNonName)
+
+    with pytest.raises(RuntimeError) as excinfo:
+        CommandGroup()
+
+    assert 'No name' in excinfo.value.message
+
+
+def test_error_same_default_alias_register():
+
+    class Subcommand(smclip.Command):
+        default_name = 'somename'
+        default_aliases = ['alias']
+
+    class CommandGroup(smclip.CommandGroup):
+
+        def __init__(self, *args, **kwargs):
+            super(CommandGroup, self).__init__(*args, **kwargs)
+
+            self.register(Subcommand)
+            self.register(Subcommand, name='othername')
+
+    with pytest.raises(RuntimeError) as excinfo:
+        CommandGroup()
+
+    assert 'already registered' in excinfo.value.message
+
+
+def test_error_same_alias_register():
+
+    class Subcommand1(smclip.Command):
+        default_name = 'somename'
+
+    class Subcommand2(smclip.Command):
+        default_name = 'somename2'
+
+    class CommandGroup(smclip.CommandGroup):
+
+        def __init__(self, *args, **kwargs):
+            super(CommandGroup, self).__init__(*args, **kwargs)
+
+            aliases = ['alias']
+            self.register(Subcommand1, aliases=aliases)
+            self.register(Subcommand2, aliases=aliases)
+
+    with pytest.raises(RuntimeError) as excinfo:
+        CommandGroup()
+
+    assert 'already registered' in excinfo.value.message
+
+
+def test_error_same_alias_as_name_register():
+
+    class Subcommand1(smclip.Command):
+        default_name = 'one'
+
+    class Subcommand2(smclip.Command):
+        default_name = 'two'
+        default_aliases = ['one']
+
+    class CommandGroup(smclip.CommandGroup):
+
+        def __init__(self, *args, **kwargs):
+            super(CommandGroup, self).__init__(*args, **kwargs)
+
+            self.register(Subcommand1)
+            self.register(Subcommand2)
+
+    with pytest.raises(RuntimeError) as excinfo:
+        CommandGroup()
+
+    assert 'already registered' in excinfo.value.message
